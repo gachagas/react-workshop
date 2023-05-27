95baa3cc0b5b031f6988a301644605cb87b92ec1
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-05-27 23:02:54 +0800

    add git-ignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline bg-red-200">Hello world! Here be an edit</h1>
     </>
   );
 }
