3f91652c77ad8292cc83988ee3a8635b923d47cf
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-05-30 20:37:39 +0800

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-200">Hello world! Here be an edit</h1>
+      <h1 className="text-3xl font-bold underline bg-red-200">Hello world! Here be an edit new edit!gi</h1>
     </>
   );
 }
