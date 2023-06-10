3b731fc4b26aa8fe089883128bee7e6529e47a81
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-06-10 16:00:46 +0800

    add handlers

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const GEMSTONES = ["aqua", "ruby", "opal", "sapphire", "jade"];
 
 export const ItemList = () => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   const message = <div>There is nothing here</div>;
 
-  const itemlist = GEMSTONES.map((datum, index) => <button>{datum}</button>);
+  const itemlist = GEMSTONES.map((datum, index) => (
+    <button
+      onClick={() => console.log(datum)}
+      className="bg-red-800"
+    >
+      {datum}
+    </button>
+  ));
 
   return (
     <>
