5528958eb19a5d8b507f267cb4b72de4901b0060
Gerlito Chagas <gerlito.chagas@gmail.com>
2023-06-10 15:55:15 +0800

    conditional rendering

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,7 +1,14 @@
 const GEMSTONES = ["aqua", "ruby", "opal", "sapphire", "jade"];
 
 export const ItemList = () => {
+  const message = <div>There is nothing here</div>;
+
   const itemlist = GEMSTONES.map((datum, index) => <button>{datum}</button>);
 
-  return <>{itemlist}</>;
+  return (
+    <>
+      {GEMSTONES.length === 0 && message}
+      {itemlist}
+    </>
+  );
 };
